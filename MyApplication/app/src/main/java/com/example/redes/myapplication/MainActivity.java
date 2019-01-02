3b708310a14d66400c66b815815bb742696d36d0
DPTO DE REDES <redes@ggedclabwrk014.espol.edu.ec>
2019-01-02 06:35:08 -0500

    Primer commit

---
+++ MyApplication/app/src/main/java/com/example/redes/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+ package com.example.redes.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+ public class MainActivity extends AppCompatActivity {
+     int numero10000 = 1000;
+ TextView resultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+     public boolean primo() {
+         int numero10000 =1000;
+         for(int i=1; i<= 1000; i++){
+
+             return = findViewById(R.id.txtResultado);
+
+         }
+         return super.isLocalVoiceInteractionSupported();
+     }
+
+}
+
